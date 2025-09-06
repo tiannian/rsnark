d9e30976b73af9f7edc6ecf3a136bcefd25e4c40
tiannian <dtiannian@gmail.com>
2025-09-07 02:59:06 +0800

    add i type support

--- core/src/circuit.rs
+++ core/src/circuit.rs
@@ -16,8 +16,11 @@ pub trait CircuitElement {
     fn append_private(&self, witness: &mut Vec<U256>);
 }
 
+#[doc(hidden)]
 pub type PrivateCircuitElement<T> = <T as CircuitElement>::Private;
+#[doc(hidden)]
 pub type PublicCircuitElement<T> = <T as CircuitElement>::Public;
+
 pub type CircuitDefine<T> = <T as CircuitElement>::Private;
 
 macro_rules! define_circuit_element_for_from_u256 {
@@ -53,4 +56,9 @@ define_circuit_element_for_from_u256!(u64);
 define_circuit_element_for_from_u256!(u32);
 define_circuit_element_for_from_u256!(u16);
 define_circuit_element_for_from_u256!(u8);
+define_circuit_element_for_from_u256!(i128);
+define_circuit_element_for_from_u256!(i64);
+define_circuit_element_for_from_u256!(i32);
+define_circuit_element_for_from_u256!(i16);
+define_circuit_element_for_from_u256!(i8);
 define_circuit_element_for_from_u256!(bool);
